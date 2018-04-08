386468e9b845e5f391b7e5aeb06c1b6725bc5e40
David Parr <dvdprr6@gmail.com>
2018-04-08 16:13:22 -0400

    constants

--- app/src/main/java/com/example/david/crudlite/utils/Constants.java
+++ app/src/main/java/com/example/david/crudlite/utils/Constants.java
@@ -3,4 +3,5 @@ package com.example.david.crudlite.utils;
 public class Constants {
     public static final String DATABASE_NAME = "database.db";
     public static final String USER_ITEMS_ARRAY = "userItemsArray";
+    public static final String EXTRA_SELECTED_USER_ITEM = "extra_selected_user_item";
 }
