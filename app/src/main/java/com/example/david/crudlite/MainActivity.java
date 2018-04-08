e3104d2f25fcc8165f9635b6001a30cb27da5415
David Parr <dvdprr6@gmail.com>
2018-04-08 11:26:15 -0400

    user activity intent

--- app/src/main/java/com/example/david/crudlite/MainActivity.java
+++ app/src/main/java/com/example/david/crudlite/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.david.crudlite;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -47,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void creatUser(View view){
-        
+        Intent userActivityIntent = new Intent(this, UserActivity.class);
+        startActivity(userActivityIntent);
     }
 
     private ArrayList<String> generateUserList(List<UserItem> allUserItems){
