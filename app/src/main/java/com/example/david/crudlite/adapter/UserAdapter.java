18806cdd132e16b7b68af82a35ecb4fa290ebaf6
David Parr <dvdprr6@gmail.com>
2018-04-08 12:31:01 -0400

    custom array adapter

---
+++ app/src/main/java/com/example/david/crudlite/adapter/UserAdapter.java
@@ -0,0 +1,44 @@
+package com.example.david.crudlite.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.david.crudlite.R;
+import com.example.david.crudlite.model.UserItem;
+
+import java.util.List;
+
+public class UserAdapter extends ArrayAdapter<UserItem>{
+
+
+    public UserAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull List<UserItem> objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup Parent){
+
+        if(convertView == null){
+            convertView = View.inflate(getContext(), R.layout.text_view, null);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        UserItem userItem = getItem(position);
+
+        String firstName = userItem.getFirstName();
+        String lastName = userItem.getLastName();
+        String email = userItem.getEmail();
+
+        sb.append("Name: " + firstName + " " + lastName + "\n" + "Email: " + email);
+
+        TextView text = convertView.findViewById(R.id.listTextView);
+        text.setText(sb.toString());
+
+
+        return convertView;
+    }
+}
