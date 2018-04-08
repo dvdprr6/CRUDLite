68877d3c69ff300a478dd0126845a63c11392fcb
David Parr <dvdprr6@gmail.com>
2018-04-08 11:24:51 -0400

    user activity

---
+++ app/src/main/java/com/example/david/crudlite/UserActivity.java
@@ -0,0 +1,51 @@
+package com.example.david.crudlite;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.david.crudlite.dao.Dao;
+import com.example.david.crudlite.dao.UserItemDao;
+import com.example.david.crudlite.model.UserItem;
+
+public class UserActivity extends AppCompatActivity {
+    private Dao<UserItem> userItemDao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        userItemDao = new UserItemDao(this);
+    }
+
+    public void submit(View view){
+        String firstName = ((EditText)findViewById(R.id.firstNameEditText)).getText().toString();
+        String lastName = ((EditText)findViewById(R.id.lastNameEditText)).getText().toString();
+        String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString().toLowerCase();
+
+        if(!firstName.equals("") && !lastName.equals("") && !email.equals("")){
+            UserItem userItem = new UserItem();
+            userItem.setUserId(null);
+            userItem.setFirstName(firstName);
+            userItem.setLastName(lastName);
+            userItem.setEmail(email);
+
+            userItemDao.insert(userItem);
+
+            Intent mainActivityIntent = new Intent(this, MainActivity.class);
+            startActivity(mainActivityIntent);
+
+        }else{
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void cancel(View view){
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(mainActivityIntent);
+    }
+}
