97d2abb0fb20c7de900abe55b1b31d50e71e3bf0
David Parr <dvdprr6@gmail.com>
2018-04-08 16:11:54 -0400

    create intent for user update activity class

--- app/src/main/java/com/example/david/crudlite/clickhandler/ListClickHandler.java
+++ app/src/main/java/com/example/david/crudlite/clickhandler/ListClickHandler.java
@@ -1,16 +1,30 @@
 package com.example.david.crudlite.clickhandler;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 
+import com.example.david.crudlite.UserUpdateActivity;
 import com.example.david.crudlite.model.UserItem;
+import com.example.david.crudlite.utils.Constants;
 
 public class ListClickHandler implements OnItemClickListener {
+    private Context context;
+
+
+    public ListClickHandler(Context context){
+        this.context = context;
+    }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         UserItem userItem = (UserItem) parent.getItemAtPosition(position);
+
+        Intent userUpdateIntent = new Intent(this.context , UserUpdateActivity.class);
+        userUpdateIntent.putExtra(Constants.EXTRA_SELECTED_USER_ITEM, userItem);
+        this.context.startActivity(userUpdateIntent);
 
     }
 }
